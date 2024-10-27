637f102afe9cfca7aa00ac99774586e39ccbdd15
qulop <qlopsimail@gmail.com>
2024-10-28 00:43:01 +0300

    (Platform) Function getPlatform() was returned

--- ruby/src/audio/IAudioOutputStream.cpp
+++ ruby/src/audio/IAudioOutputStream.cpp
@@ -9,12 +9,12 @@
 
 namespace Ruby {
 	Ptr<IAudioOutputStream> IAudioOutputStream::Create(const AudioParams& params) {
-#ifdef RUBY_WIN32_USED
-	    return MakePtr<Win32::WaveOutAudioOutputStream>(params);
-#else
-        RUBY_CRITICAL("IAudioOutputStream::Create() : Failed to create instance of IAudioOutputStream -- your platform isn't supported for now");
-
-        return nullptr; // Unreachable code#
-#endif
+        if constexpr (getPlatform() == PLATFORM_WINDOWS) {   // NOLINT
+            return MakePtr<Win32::WaveOutAudioOutputStream>(params);
+        } else {
+            RUBY_CRITICAL(
+                "IAudioOutputStream::Create() : Failed to create instance of IAudioOutputStream -- your platform isn't supported for now");
+            return nullptr; // Unreachable code
+        }
     }
 }
--- ruby/src/platform/Platform.hpp
+++ ruby/src/platform/Platform.hpp
@@ -7,11 +7,27 @@
 #endif
 
 namespace Ruby {
+    enum CurrentPlatform {
+        PLATFORM_UNKNOWN,
+        PLATFORM_WINDOWS,
+        PLATFORM_LINUX
+    };
+
     enum WindowVendor {
         VENDOR_UNKNOWN,
         VENDOR_GLFW,
         VENDOR_WIN32
     };
+
+    constexpr CurrentPlatform getPlatform() noexcept {
+        #ifdef RUBY_WIN32_USED
+            return PLATFORM_WINDOWS;
+        #elif defined(RUBY_LINUX_USED)
+            return PLATFORM_LINUX;
+        #else
+            return PLATFORM_UNKNOWN;
+        #endif
+    }
 
     template<typename... Args>
     void errorBoxF(std::format_string<Args...> fmt, const RubyString& title, Args&&... args) noexcept {
