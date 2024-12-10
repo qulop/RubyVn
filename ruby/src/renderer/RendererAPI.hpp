ee07af8d21c9b01ad83dbd12d9c8c0468fdeb39e
qulop <qlopsimail@gmail.com>
2024-12-10 12:23:12 +0300

    (Renderer) Added more API methods for RendererAPI

--- ruby/src/renderer/RendererAPI.hpp
+++ ruby/src/renderer/RendererAPI.hpp
@@ -7,14 +7,36 @@
 #include "VertexArray.hpp"
 
 
-namespace Ruby::RendererAPI {
-    void RUBY_API setViewport(u32 x, u32 y, u32 width, u32 height);
+namespace Ruby {
+    enum class GraphicAPIName {
+        NONE,
+        OPENGL, VULKAN, DIRECTX
+    };
 
-    void RUBY_API clearBuffers();
-    void RUBY_API clearColor(const Color& color);
-    void RUBY_API clearColor(f32 r, f32 g, f32 b, f32 a);
 
-    void RUBY_API drawIndexed(const VertexArray& vao, u32 indexCount=0);
-    void RUBY_API drawLines(const VertexArray& vao, u32 vertexCount);
-    void RUBY_API setLineWidth(f32 width);
+    class RendererAPI {
+    public:
+        using ErrorType = GLenum;
+
+        static void Init(u32 width, u32 height);
+
+        static String GetVersionOfAPI();
+        static String GetVendor();
+        static String GetRendererName();
+        static GraphicAPIName GetNameOfAPI();
+        static u32 GetShadingLanguageVersion();
+        static ErrorType GetError();
+
+        static i32 GetMaxVertexAttribs();
+
+        static void SetViewport(u32 x, u32 y, u32 width, u32 height);
+
+        static void ClearBuffers();
+        static void ClearColor(const Color& color);
+        static void ClearColor(f32 r, f32 g, f32 b, f32 a);
+
+        static void DrawElements(const VertexArray& vao, u32 indexCount=0);
+        static void DrawLines(const VertexArray& vao, u32 vertexCount);
+        static void SetLineWidth(f32 width);
+    };
 }
