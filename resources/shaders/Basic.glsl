56091658b5afaaf2a2591339ba657c2a30d8d0a9
qulop <qlopsimail@gmail.com>
2025-01-08 09:09:34 +0300

    Happy New Year!

--- resources/shaders/Basic.glsl
+++ resources/shaders/Basic.glsl
@@ -1,3 +1,4 @@
+#type vertex
 #version 330 core
 
 layout (location = 0) in vec3 pos;
@@ -9,9 +10,24 @@ out vec2 fragmentTextureCoords;
 
 uniform float offset;
 
-void main()
-{
+void main() {
     gl_Position = vec4(pos.x + 10, pos.y + 10, pos.z, 1.0f);
     fragmentColor = color;
     fragmentTextureCoords = textureCoords;
+}
+
+#type fragment
+#version 330 core
+
+in vec3 fragmentColor;
+in vec2 fragmentTextureCoords;
+
+out vec4 fragColor;
+
+uniform sampler2D resTexture;
+uniform vec3 col;
+
+void main() {
+    // fragColor = texture(resTexture, fragmentTextureCoords);
+    fragColor = vec4(col, 1.0f);
 }
